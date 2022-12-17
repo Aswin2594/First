a72e34f52f282516137720f5261cdb8911fe8f19
aswin <aswin@192.168.1.45>
2022-12-17 16:37:08 +0530

    First commit
    uploading all files

---
+++ ja11/src/Array/C1.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class C1 {
+	//create array
+	
+	public static void main(String [] args) {
+		int rollNumber[] = {101, 102, 103, 700};
+		System.out.println(rollNumber [2]);
+		System.out.println("Count :" +rollNumber.length);
+	
+		//entire array
+	 for(int index =0; index <rollNumber.length; index=index+1) {
+	 }
+	System.out.println("rollNumbers[index]");
+	{
+	}
+	}
+}
---
+++ ja11/src/Array/c2.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.ArrayList;
+
+public class c2 {
+	public static void main (String[] args) {
+		ArrayList<String> Fruits = new ArrayList<String>();
+		
+		Fruits.add("Apple");
+		Fruits.add("Banana");
+		Fruits.add("Orange");
+		 System.out.println(Fruits);
+		
+		 //size of arraylist
+		 System.out.println(Fruits.size());
+		 
+		 //update array list
+		 Fruits.set(1, "kivi");
+		 System.out.println(Fruits);
+		 
+		 //remove from array list
+		 Fruits.remove(2);
+		 System.out.println(Fruits);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
